3995eb3687d49bed2a3bbac58626126cf6b457f8
Yousef <36880618+yusnof@users.noreply.github.com>
2024-01-25 09:49:52 +0100

    Start of lab2

---
+++ Scania.java
@@ -0,0 +1,22 @@
+public class Scania extends Car {
+
+    double truckBedAngel;
+
+
+    Scania(){
+        modelName = "Scania";
+        nrDoors = 2;
+        stopEngine();
+    }
+
+    public void decrementTruckBedAngel(){
+    //TODO
+    }
+    public void incrementTruckBedAngel(){
+     //TODO
+    }
+    public double getTruckBedAngel(){
+        return this.truckBedAngel;
+    }
+
+}
